d1133893fd30ccbdd0f748eac3d897055e432812
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-06-19 14:17:22 +0700

    commit 9 update code file StaffDetailComponent.js

--- src/components/StaffDetailComponent.js
+++ src/components/StaffDetailComponent.js
@@ -3,7 +3,30 @@ import { Breadcrumb, BreadcrumbItem, Card,CardBody,CardImg,CardText,CardTitle} f
 import dateFormat from 'dateformat';
 import {Link} from 'react-router-dom';
 
-/* hàm render trả chi tiết thồng tin của nhân viên */
+/* hàm render chi tiết thồng tin của nhân viên */
+function RenderStaffDetail({staff1}){
+    return(
+        <div className='col-12 row'>
+            <div className='mt-4 left col-lg-3 col-md-4 col-6'> 
+                <CardImg  src={staff1.image}/>     
+            </div>
+            <div className='mt-1 right col-lg-9 col-md-8 col-6'>
+                <CardBody>
+                    <CardTitle>Họ và tên: <b>{staff1.name}</b> </CardTitle>
+                    <CardText>Ngày sinh: {dateFormat(staff1.doB,'dd/mm/yyyy')}</CardText>
+                    <CardText>Ngày vào công ty: {dateFormat(staff1.startDate,'dd/mm/yyyy')}</CardText>
+                    <CardText>Phòng ban: {staff1.department.name}</CardText>
+                    <CardText>Bậc lương: {staff1.salaryScale}</CardText>
+                    <CardText>Số ngày nghỉ còn lại: {staff1.annualLeave}</CardText>
+                    <CardText>số ngày đã làm thêm: {staff1.overTime}</CardText>
+                </CardBody>
+            </div> 
+
+        </div>
+    );
+}
+
+/* hàm render toàn bộ trang chi tiết thồng tin của nhân viên */
 function StaffDetail(props){    
     
         const staff1=props.staff    
@@ -19,32 +42,15 @@ function StaffDetail(props){
                         <hr/>
                     </div>
                     <Card className=''>
-                    <div className="container row"key={staff1.id}>
-                        <div className='mt-4 left col-lg-3 col-md-4 col-6'> 
-                            <CardImg  src={staff1.image}/>     
-                        </div>
-                        <div className='mt-1 right col-lg-9 col-md-8 col-6'>
-                            <CardBody>
-                                 <CardTitle>Họ và tên: <b>{staff1.name}</b> </CardTitle>
-                                 <CardText>Ngày sinh: {dateFormat(staff1.doB,'dd/mm/yyyy')}</CardText>
-                                 <CardText>Ngày vào công ty: {dateFormat(staff1.startDate,'dd/mm/yyyy')}</CardText>
-                                 <CardText>Phòng ban: {staff1.department.name}</CardText>
-                                 <CardText>Bậc lương: {staff1.salaryScale}</CardText>
-                                 <CardText>Số ngày nghỉ còn lại: {staff1.annualLeave}</CardText>
-                                 <CardText>số ngày đã làm thêm: {staff1.overTime}</CardText>
-                            </CardBody>
-                        </div>                        
-                    </div>                         
-                </Card>
-
-                </div>
-                
+                         <div className="container row"key={staff1.id}>
+                             <RenderStaffDetail staff1={staff1}/>                                               
+                         </div>                         
+                    </Card>
+                </div>                
             ); 
         else {
-            return(<div></div>);
-            
+            return(<div></div>);            
         }   
-    }
-    
+    }   
 
 export default StaffDetail;
