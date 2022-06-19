f2c7cab11822a8d45920109e008e64923b7bb564
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-06-19 14:04:04 +0700

    commit 8 update code file StaffListComponent.js

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,34 +1,42 @@
 
-import React/* ,{Component} */ from 'react';
-/* import StaffDetail from './StaffDetailComponent'; */
+import React from 'react';
 import {Card,CardImg,CardBody,CardTitle}from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-/* hàm StaffList render ra phần body trang */
+
+
+/* hàm Render ra ảnh và tên nhân viên */
+function RenderStaffList({staff}){
+    return(
+        <div>
+           <Card key={staff.id} >
+                <Link to ={`/staff/${staff.id}`}>
+                    <CardImg src={staff.image} alt={staff.name}/>
+                    <CardBody>
+                        <CardTitle>{staff.name}</CardTitle>
+                    </CardBody>
+                </Link>
+                        
+            </Card>
+        </div>
+    );
+
+}
+/* hàm StaffList render ra danh sách nhân viên */
 function StaffList(props){
-    
-/* hàm render hiển thị ra danh sách các nhân viên */
-   
+
      console.log(props.staff);
         const stafflist=props.staff.map((staff)=>{
             return(
                 <div className="col-6 col-md-4 col-lg-2 mt-3">
-                    <Card key={staff.id} >
-                        <Link to ={`/staff/${staff.id}`}>
-                            <CardImg src={staff.image} alt={staff.name}/>
-                            <CardBody>
-                                 <CardTitle>{staff.name}</CardTitle>
-                             </CardBody>
-                        </Link>
-                        
-                    </Card>
+                    <RenderStaffList staff={staff}/>
                 </div>                
             );
         });
         return(
             <div class="container">  
-                <div className='row col-12'>
-                    <h2>nhân viên</h2>
+                <div className='col-12'>
+                    <h2>Nhân viên</h2>
                     <hr/>                    
                 </div>             
                 <div class="row">
