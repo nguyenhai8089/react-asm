0a0ed9ffbe210290b4fd7cd7c3982e11cb562478
nguyen phu hai <nguyenphuhai89@gmail.com>
2022-10-19 10:44:34 +0700

    commit update chú thích văn bản

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,7 +1,20 @@
 
 import React,{Component} from 'react';
-import {Card,CardImg,CardBody,CardTitle,Button,Input,Modal, ModalHeader, ModalBody,Form
-         , FormGroup,Col,Label, FormFeedback }from 'reactstrap';
+import {
+    Card,
+    CardImg,
+    CardBody,
+    CardTitle,
+    Button,
+    Input,
+    Modal, 
+    ModalHeader, 
+    ModalBody,
+    Form,
+    FormGroup,
+    Col,Label,
+    FormFeedback
+    }from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { DEPARTMENTS } from "../shared/staffs";
 
@@ -60,18 +73,27 @@ class StaffList extends Component{
         event.preventDefault();       
         const NameS1= this.NameSearch.value;        
         console.log("giá trị của NameSearch ="+NameS1);
+
         /* hàm lọc nhân viên */
         const staffList=this.props.staff        
-        .filter((val) => val.name.toLowerCase().includes(NameS1.toLowerCase()));
-            if(staffList.length!==0) 
-                return this.setState({newStaffFilter:staffList});
-            else
-                return this.setState({newStaffFilter:[...this.props.staff]});
+        .filter(
+                (val) => val.name.toLowerCase().includes(NameS1.toLowerCase())
+            );
+        if(staffList.length!==0) 
+            return this.setState(
+                {newStaffFilter:staffList}
+            );
+        else
+            return this.setState(
+                {newStaffFilter:[...this.props.staff]}
+            );
         
     }
     /* hàm sự kiện khi người dùng bấm vào ô input mà không nhập dữ liệu */
     handleBlur=(field)=>(event)=>{
-        this.setState({touched:{...this.state.touched,[field]:true}});
+        this.setState(
+            {touched:{...this.state.touched,[field]:true}}
+        );
     }
     /* hàm sự kiện handleInputChange nhận dữ liệu nhập vào */
     handleInputChange(event){
@@ -81,7 +103,14 @@ class StaffList extends Component{
     }
     /* hàm sự kiện handleSubmit khi người dùng thêm nhân viên */
     handleSubmit=(event)=>{
-        const errors =this.valiDate(this.state.name,this.state.doB,this.state.startDate,this.state.department,this.state.salaryScale,this.state.annualLeave,this.state.overTime        
+        const errors =this.valiDate(
+            this.state.name,
+            this.state.doB,
+            this.state.startDate,
+            this.state.department,
+            this.state.salaryScale,
+            this.state.annualLeave,
+            this.state.overTime        
             );
         event.preventDefault();
         const department = DEPARTMENTS.find(
@@ -100,16 +129,30 @@ class StaffList extends Component{
             image:'/assets/images/alberto.png'            
         };
         //điều kiện hoàn thành form thêm nhân viên
-        if ((newStaff.name==="")||(newStaff.doB==="")||(newStaff.startDate==="")||(newStaff.department==="")||(newStaff.salaryScale==="")||(newStaff.annualLeave==="")||(newStaff.overTime==="")||
-            (errors.name!=="") ||(errors.doB!=="")||(errors.salaryScale!=="")||(errors.department!=="")||(errors.annualLeave!=="")||(errors.overTime!=="") 
+        if (
+            (newStaff.name==="")||
+            (newStaff.doB==="")||
+            (newStaff.startDate==="")||
+            (newStaff.department==="")||
+            (newStaff.salaryScale==="")||
+            (newStaff.annualLeave==="")||
+            (newStaff.overTime==="")||
+            (errors.name!=="") ||
+            (errors.doB!=="")||
+            (errors.salaryScale!=="")||
+            (errors.department!=="")||
+            (errors.annualLeave!=="")||
+            (errors.overTime!=="") 
             )
-        alert("Vui lòng nhập đầy đủ thông tin các trường theo hướng dẫn")
+             alert("Vui lòng nhập đầy đủ thông tin các trường theo hướng dẫn")
         else
-        this.props.onNewStaff(newStaff);
+              this.props.onNewStaff(newStaff);
     }
     /* hàm đóng mở form thêm nhân viên */
     toggleModal(){
-        this.setState({isModalOpen:!this.state.isModalOpen});
+        this.setState(
+            {isModalOpen:!this.state.isModalOpen}
+        );
     }
    /*  hàm xác thực form thêm nhân viên  */
     valiDate(name,doB,startDate,department,salaryScale,annualLeave,overTime){
@@ -123,40 +166,76 @@ class StaffList extends Component{
             overTime:''        
         }
         /* xác thực tên nhân viên */
-        if(this.state.touched.name&&name.length<3)
+        if(
+            this.state.touched.name&&name.length<3
+            )
             {errors.name="Họ và tên phải có từ 3 kí tự";}
-        else if(this.state.touched.name&&name.length>30)
+        else if(
+            this.state.touched.name&&name.length>30
+            )
             {errors.name="Họ và tên phải ít hơn hoặc bằng 20 ký tự"; }
+
         /* xác thực ngày tháng năm sinh */
-        if(this.state.touched.doB&&doB.length<1)
+        if(
+            this.state.touched.doB&&doB.length<1
+            )
             {errors.doB="Ngày tháng năm sinh không được bỏ trống";}
+
         /* xác thực ngày vào công ty */
-        if(this.state.touched.startDate&&startDate.length<1)
+        if(
+            this.state.touched.startDate&&startDate.length<1
+            )
             {errors.startDate="Ngày vào công ty không được bỏ trống";}
+
         /* xác thực phòng ban */
         if(department==='')
             {errors.department="Phòng ban không được bỏ trống";} 
+
         /* xác thực hệ số lương */
-        if((this.state.touched.salaryScale&&salaryScale>4)||(this.state.touched.salaryScale&&salaryScale<1))
+        if(
+            (this.state.touched.salaryScale&&salaryScale>4)||
+            (this.state.touched.salaryScale&&salaryScale<1)
+            )
             {errors.salaryScale="Hệ số lương phải có giá trị từ 1.0 ->4.0";}
-        else if(this.state.touched.salaryScale&&salaryScale.split('').filter((x)=>x==='.').length!==1)
+        else if(
+            this.state.touched.salaryScale&&salaryScale.split('').filter((x)=>x==='.').length!==1
+            )
             {errors.salaryScale="Hệ số lương phải có dấu chấm ở giữa (ví dụ 2.5)";}
+
         /* xác thực số ngày nghỉ còn lại */
-        if((this.state.touched.annualLeave&&annualLeave>12)||(this.state.touched.annualLeave&&annualLeave<0))
+        if(
+            (this.state.touched.annualLeave&&annualLeave>12)||
+            (this.state.touched.annualLeave&&annualLeave<0)
+            )
             {errors.annualLeave="Số ngày nghỉ còn lại phải có giá trị từ 0.0 ->12.0";}
-        else if(this.state.touched.annualLeave&&annualLeave.split('').filter((x)=>x==='.').length!==1)
+        else if(
+            this.state.touched.annualLeave&&annualLeave.split('').filter((x)=>x==='.').length!==1
+            )
             {errors.annualLeave="Số ngày nghỉ phải là số có chứa dấu chấm ở giữa (ví dụ 8.5)";}
+
         /* xác thực số ngày đã tăng ca */
-        if((this.state.touched.overTime&&overTime>30)||(this.state.touched.overTime&&overTime<0))
+        if(
+            (this.state.touched.overTime&&overTime>30)||
+            (this.state.touched.overTime&&overTime<0)
+            )
             {errors.overTime="Số ngày làm thêm phải có giá trị từ 0.0 ->30.0";}
-        else if(this.state.touched.overTime&&overTime.split('').filter((x)=>x==='.').length!==1)
+        else if(
+            this.state.touched.overTime&&overTime.split('').filter((x)=>x==='.').length!==1
+            )
             {errors.overTime="Số ngày làm thêm phải là số có chứa dấu chấm ở giữa (ví dụ 9.5)";}
         
         return errors; 
     }
 
     render(event){
-        const errors =this.valiDate(this.state.name,this.state.doB,this.state.startDate,this.state.department,this.state.salaryScale,this.state.annualLeave,this.state.overTime        
+        const errors =this.valiDate(
+            this.state.name,
+            this.state.doB,
+            this.state.startDate,
+            this.state.department,
+            this.state.salaryScale,
+            this.state.annualLeave,
+            this.state.overTime        
             );
         console.log("giá trị của newStaffFilter="+this.state.newStaffFilter);
         console.log("giá trị của staff="+this.props.staff);
@@ -211,6 +290,7 @@ class StaffList extends Component{
                     </ModalHeader>
                     <ModalBody>
                         <Form onSubmit={this.handleSubmit}>
+                            {/*  Họ và tên: */}
                             <FormGroup row>
                                 <Label md={3}>
                                     Họ và tên:
@@ -231,6 +311,7 @@ class StaffList extends Component{
                                     </FormFeedback>
                                 </Col>
                             </FormGroup>
+                            {/* Ngày tháng năm sinh: */}
                             <FormGroup row>
                                 <Label md={3}>
                                     Ngày tháng năm sinh:
@@ -250,6 +331,7 @@ class StaffList extends Component{
                                     </FormFeedback>
                                 </Col>
                             </FormGroup>
+                            {/* Ngày bắt đầu: */}
                             <FormGroup row>
                                 <Label md={3}>
                                     Ngày bắt đầu:
@@ -269,7 +351,7 @@ class StaffList extends Component{
                                     </FormFeedback>
                                 </Col>
                             </FormGroup>
-                            
+                            {/* Phòng ban: */}
                             <FormGroup row>
                                 <Label md={3}>
                                     Phòng ban:
@@ -296,6 +378,7 @@ class StaffList extends Component{
                                     </FormFeedback> 
                                 </Col>
                             </FormGroup>
+                            {/* Hệ số lương: */}
                             <FormGroup row>
                                 <Label md={3}>
                                     Hệ số lương:
@@ -316,6 +399,7 @@ class StaffList extends Component{
                                     </FormFeedback>
                                 </Col>
                             </FormGroup>
+                            {/* Số ngày nghỉ còn lại: */}
                             <FormGroup row>
                                 <Label md={3}>
                                     Số ngày nghỉ còn lại:
@@ -336,6 +420,7 @@ class StaffList extends Component{
                                     </FormFeedback>
                                 </Col>
                             </FormGroup>
+                            {/* Số ngày tăng ca: */}
                             <FormGroup row>
                                 <Label md={3}>
                                     Số ngày tăng ca:
@@ -356,6 +441,7 @@ class StaffList extends Component{
                                     </FormFeedback>
                                 </Col>
                             </FormGroup>
+                            {/* Submit */}
                             <FormGroup row>
                                 <Col md={{size:5,offset:5}} >
                                     <Button type='submit' name='submit' color='primary'onClick={this.toggleModal}>
